fad6b21495aca2d672a1dbb6874cfc353b2853b4
HongSukJu <ehensnfl@kookmin.ac.kr>
2020-11-04 16:44:56 +0900

    상단바 제거, 두 Activity 추가
    activity_freshness.xml - 신선도 품목
    activity_fridge.xml - 냉장고 종합

---
+++ app/src/main/java/com/example/najakneang/FreshnessActivity.java
@@ -0,0 +1,14 @@
+package com.example.najakneang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FreshnessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freshness);
+    }
+}
