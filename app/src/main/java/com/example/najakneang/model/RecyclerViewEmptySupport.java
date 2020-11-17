c3574117e1b93c3e3461870b41bc926baa897595
Hong <ehensnfl@kookmin.ac.kr>
2020-11-17 15:37:49 +0900

    RecyclerViewEmptySupport 제작 후 FreshnessActivity 적용

---
+++ app/src/main/java/com/example/najakneang/model/RecyclerViewEmptySupport.java
@@ -0,0 +1,60 @@
+package com.example.najakneang.model;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.najakneang.activity.FreshnessActivity;
+
+public class RecyclerViewEmptySupport extends RecyclerView {
+
+    private View emptyView;
+    private final AdapterDataObserver emptyObserver = new AdapterDataObserver() {
+
+        @Override
+        public void onChanged() {
+            Adapter<?> adapter = getAdapter();
+            if (adapter != null && emptyView != null) {
+                if (adapter.getItemCount() == 1) {
+                    emptyView.setVisibility(View.VISIBLE);
+                    RecyclerViewEmptySupport.this.setVisibility(View.GONE);
+                } else {
+                    emptyView.setVisibility(View.INVISIBLE);
+                    RecyclerViewEmptySupport.this.setVisibility(View.VISIBLE);
+                }
+            }
+
+        }
+    };
+
+    public RecyclerViewEmptySupport(Context context) {
+        super(context);
+    }
+
+    public RecyclerViewEmptySupport(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RecyclerViewEmptySupport(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        super.setAdapter(adapter);
+
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(emptyObserver);
+        }
+
+        emptyObserver.onChanged();
+    }
+
+    public void setEmptyView(View emptyView) {
+        this.emptyView = emptyView;
+    }
+}
