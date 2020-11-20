613fe2d0cccebbde91da34ec28e6af3bd254e544
mclub4 <mclub4@kookmin.ac.kr>
2020-11-20 18:24:22 +0900

    Dialog, Activity 및 Adapter 일부 주석 추가

--- app/src/main/java/com/example/najakneang/adapter/RecommendRecyclerAdapter.java
+++ app/src/main/java/com/example/najakneang/adapter/RecommendRecyclerAdapter.java
@@ -17,12 +17,17 @@ import com.example.najakneang.R;
 
 import java.util.ArrayList;
 
+// 파일 설명 : 추천 레시피 RecyclerView 와 보여줄 데이터를 연결하는 Adapter class
+// 파일 주요 기능 : RecyclerView의 각 홀더에 데이터 및 이벤트 연결
+
+// 클래스 설명 : RecyclerView의 각 View를 저장하는 Holder 객체 class
 class RecommendRecyclerHolder extends RecyclerView.ViewHolder {
     protected final View view;
     protected final ImageView thumbnail;
     protected final TextView title;
     protected final TextView creator;
 
+    // 메서드 설명 : Holder 객체를 생성하고 item_recycler_recommend 레이아웃의 View와 연결
     public RecommendRecyclerHolder(@NonNull View view) {
         super(view);
         this.view = view;
@@ -32,15 +37,18 @@ class RecommendRecyclerHolder extends RecyclerView.ViewHolder {
     }
 }
 
+// 클래스 설명 : RecyclerView와 데이터를 연결하는 Adapter class
 public class RecommendRecyclerAdapter
         extends RecyclerView.Adapter<RecommendRecyclerHolder> {
 
     private final ArrayList<YoutubeContent> data;
 
+    // 메서드 설명 : 입력받은 유튜브 영상 정보를 담은 객체를 저장
     public RecommendRecyclerAdapter(ArrayList<YoutubeContent> data) {
         this.data = data;
     }
 
+    // 메서드 설명 : Holder 객체를 item_recycler_recommend 레이아웃에 따라 생성
     @NonNull
     @Override
     public RecommendRecyclerHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
@@ -50,8 +58,10 @@ public class RecommendRecyclerAdapter
         return new RecommendRecyclerHolder(view);
     }
 
+    // 메서드 설명 : 생성된 Holder 객체와 유튜브 정보 객체를 연결
     @Override
     public void onBindViewHolder(@NonNull RecommendRecyclerHolder holder, int position) {
+        // Recycler 각 Item에 제목, 채널명, 썸네일 지정
         YoutubeContent datum = data.get(position);
         String title = datum.getTitle();
         String creator = datum.getCreator();
@@ -60,6 +70,8 @@ public class RecommendRecyclerAdapter
         holder.title.setText(title);
         holder.creator.setText(creator);
         holder.thumbnail.setImageBitmap(thumbnail_bitmap);
+
+        // Item 클릭시, 해당 유튜브 영상으로 이동
         holder.view.setOnClickListener(view -> {
             String videoId = datum.getVideoId();
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse( "http://youtube.com/watch?v=" + videoId ));
--- app/src/main/java/com/example/najakneang/model/GoodsDialog.java
+++ app/src/main/java/com/example/najakneang/model/GoodsDialog.java
@@ -29,6 +29,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+// 파일 설명 : 재료 추가 및 수정 Dialog의 기능을 구성하는 파일
+// 파일 주요기능 : 사용자가 Dialog에 입력한 정보를 바탕으로 유효한지 검토하고 DB에 저장 및 수정
+
+// 클래스 설명 : Dialog의 EditText, Spinner에서 입력한 정보를 검토하고 DB에 저장 및 수정
 
 public class GoodsDialog extends Dialog implements View.OnClickListener {
 
@@ -52,11 +56,15 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
 
     SQLiteDatabase db = MainActivity.db;
 
+    // 메서드 설명 : GoodsDialog 객체를 생성하고 Dialog가 생선된 Activity의 Context와 연결
+    // (FreshnessActivity에서 재료를 추가할 경우)
     public GoodsDialog(@NonNull Context context){
         super(context);
         this.context = context;
     }
 
+    // 메서드 설명 : GoodsDialog 객체를 생성하고 Dialog가 생선된 Activity의 Context, 수정할 재료의 id, 이름, 수량, 유통기한을 받아옴
+    // (GoodsActivity에서 재료 수정할 경우)
     public GoodsDialog(@NonNull Context context, Long id, String nameStr, String quantityStr, String expireDateStr){
         super(context);
         this.context = context;
@@ -66,7 +74,8 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
         this.expireDateStr = expireDateStr;
     }
 
-
+    // 메서드 설명 : GoodsDialog 객체를 생성하고 Dialog가 생선된 Activity의 Context, 해당 구역의 냉장고, 이름, 보관 상태를 받아옴
+    // (SectionActivity에서 재료를 추가할 경우)
     public GoodsDialog(@NonNull Context context, String fridge, String section, String fridgeCategory) {
         super(context);
         this.context = context;
@@ -79,19 +88,21 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        //Dialog 실행시, 주변 화면 흐리게 하기
+        // Dialog 실행시, 주변 화면 흐리게 하기
         WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
         layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         layoutParams.dimAmount = 0.8f;
         getWindow().setAttributes(layoutParams);
 
-        //Dialog 레이아웃 지정
+        // Dialog 레이아웃 지정
         setContentView(R.layout.dialog_goods);
 
         setupDialog();
     }
 
+    // 메서드 설명 : Dialog를 실행된 Activity에 따라 설정해주는 함수
     private void setupDialog() {
+        // Dialog의 Widget을 id로 받아옴
         TextView okBtn = findViewById(R.id.btn_ok_goods_dialog);
         TextView cancelBtn = findViewById(R.id.btn_cancel_goods_dialog);
         name = findViewById(R.id.edit_name_goods_dialog);
@@ -103,10 +114,14 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
         fridgeLayout = findViewById(R.id.fridge_layout_goods_dialog);
         dialogText = findViewById(R.id.dialogText);
 
+        // 확인, 취소 버튼을 클릭 리스너와 연결
         okBtn.setOnClickListener(this);
         cancelBtn.setOnClickListener(this);
 
+        // Dialog가 FreshnessActivity나 GoodsActivity에서 실행된 경우
+        // (구역에서 재료추가할 경우, 냉장고 및 구역 선택이 안보이게 하기 위함)
         if (context.getClass() == FreshnessActivity.class || context.getClass() == GoodsActivity.class) {
+            // 생성된 냉장고들 이름을 받아와서 ArrayList에 저장
             ArrayList<String> fridgeNameList = new ArrayList<>();
             Cursor cursor = db.query(
                     DBContract.FridgeEntry.TABLE_NAME,
@@ -124,6 +139,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
             }
             cursor.close();
 
+            // GoodsActivity에서 생성된 경우는 재료 수정, 아니면 재료 추가
             if((context.getClass() == GoodsActivity.class)){
                 name.setText(nameStr);
                 quantity.setText(quantityStr);
@@ -134,13 +150,17 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                 dialogText.setText("재료 추가하기");
             }
 
+            // 냉장고 Spinner 데이터 설정
             final ArrayAdapter<String> fridgeAdapter = new ArrayAdapter<>(context,
                     android.R.layout.simple_spinner_item, fridgeNameList);
             fridgeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             fridgeSpinner.setAdapter(fridgeAdapter);
+
+            // 냉장고 Spinner에 Item이 선택됐을 경우, 그 냉장고의 구역 받아오기
             fridgeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
                 public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    // 선택한 냉장고의 종류 받아오기
                     Cursor fridgeCursor = db.query(
                             DBContract.FridgeEntry.TABLE_NAME,
                             new String[]{ DBContract.FridgeEntry.COLUMN_CATEGORY },
@@ -156,6 +176,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                             fridgeCursor.getColumnIndex(DBContract.FridgeEntry.COLUMN_CATEGORY));
 
                     final ArrayAdapter<CharSequence> newTypeAdapter;
+                    // 냉장고가 와인 냉장고일 경우, 재료 종류는 주류만 넣을 수 있게 만듦
                     if (category.equals("와인 냉장고")) {
                         newTypeAdapter = ArrayAdapter.createFromResource(context,
                                 R.array.alchoholArray, android.R.layout.simple_spinner_item);
@@ -167,7 +188,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                     typeSpinner.setAdapter(newTypeAdapter);
                     typeSpinner.setSelection(0);
 
-
+                    // 선택한 냉장고의 구역 받아오기
                     ArrayList<String> sectionNameList = new ArrayList<>();
                     Cursor cursor = db.query(
                             DBContract.SectionEntry.TABLE_NAME,
@@ -179,6 +200,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                             null
                     );
 
+                    // 선택한 냉장고에 아무 구역이 없을 경우
                     if (cursor.getCount() == 0)
                         Toast.makeText(context, "구역이 없는 냉장고입니다.", Toast.LENGTH_SHORT).show();
 
@@ -188,6 +210,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                     }
                     cursor.close();
 
+                    // 구역 Spinner에 데이터 적용
                     final ArrayAdapter<String> sectionAdapter = new ArrayAdapter<>(context,
                             android.R.layout.simple_spinner_item, sectionNameList);
                     sectionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -201,6 +224,8 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
 
             fridgeSpinner.setSelection(0,false);
         } else {
+            // Dialog가 SectionActivity에서 생성된 경우
+            dialogText.setText("재료 추가하기");
             final ArrayAdapter<CharSequence> typeAdapter;
             if (fridgeCategory.equals("와인 냉장고")) {
                 typeAdapter = ArrayAdapter.createFromResource(context,
@@ -213,6 +238,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
             typeSpinner.setAdapter(typeAdapter);
             typeSpinner.setSelection(0);
 
+            // SectionActivity에서 재료 추가는 냉장고 및 구역 Spinner는 보이지 않음
             fridgeLayout.setVisibility(View.GONE);
         }
     }
@@ -225,31 +251,45 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                 String quantityStr = quantity.getText().toString();
                 String expireDateStr = expireDate.getText().toString();
 
+                // 냉장고가 선택되지 않았을 경우 (FreshnessActivity랑 GoodsActivity만)
                 if (fridgeSpinner.getSelectedItemPosition() == -1 && fridgeLayout.getVisibility() == View.VISIBLE) {
                     Toast.makeText(context.getApplicationContext(),
                             "냉장고를 선택해주세요",Toast.LENGTH_SHORT).show();
-                } else if (sectionSpinner.getSelectedItemPosition() == -1 && fridgeLayout.getVisibility() == View.VISIBLE) {
+                }
+                // 구역이 선택되지 않았을 경우 (FreshnessActivity랑 GoodsActivity만)
+                else if (sectionSpinner.getSelectedItemPosition() == -1 && fridgeLayout.getVisibility() == View.VISIBLE) {
                     Toast.makeText(context.getApplicationContext(),
                             "구역을 선택해주세요", Toast.LENGTH_SHORT).show();
-                } else if (nameStr.trim().getBytes().length == 0) {
+                }
+                // 이름이 공백일 경우
+                else if (nameStr.trim().getBytes().length == 0) {
                     Toast.makeText(context.getApplicationContext(),
                             "이름을 입력해주세요",Toast.LENGTH_SHORT).show();
-                } else if (quantityStr.trim().getBytes().length == 0) {
+                }
+                // 수량이 공백일 경우
+                else if (quantityStr.trim().getBytes().length == 0) {
                     Toast.makeText(context.getApplicationContext(),
                             "수량을 입력해주세요", Toast.LENGTH_SHORT).show();
-                } else if (typeSpinner.getSelectedItemPosition() == -1) {
+                }
+                // 재료 종류가 선택되지 않았을 경우
+                else if (typeSpinner.getSelectedItemPosition() == -1) {
                     Toast.makeText(context.getApplicationContext(),
                             "종류를 선택해주세요", Toast.LENGTH_SHORT).show();
                 } else {
                     try {
+                        // 날짜가 입력되지 않거나 형식이 잘못됐을 경우 Exception 발생
                         LocalDate expireDate = LocalDate.parse(expireDateStr, DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+                        // 수량이 Int 범위를 초과한 경우, Exception 발생
                         int quantityInt = Integer.parseInt(quantityStr);
 
+                        // 수량이 음수거나 터무니 없이 큰 경우
                         if(quantityInt>10000 || quantityInt<=0){
                             Toast.makeText(context.getApplicationContext(),
                                     "수량 형식이 잘못되었습니다", Toast.LENGTH_SHORT).show();
                         }
 
+                        // 입력한 데이터 DB에 저장
                         ContentValues values = new ContentValues();
                         values.put(DBContract.GoodsEntry.COLUMN_NAME, nameStr);
                         values.put(DBContract.GoodsEntry.COLUMN_QUANTITY, quantityInt);
@@ -264,11 +304,13 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                         values.put(DBContract.GoodsEntry.COLUMN_SECTION,
                                 fridgeLayout.getVisibility() == View.GONE? section:sectionSpinner.getSelectedItem().toString());
 
+                        // FreshnessActivity와 SectionActivity는 재료 추가
                         if(context.getClass() != GoodsActivity.class) {
                             db.insert(DBContract.GoodsEntry.TABLE_NAME, null, values);
                             Toast.makeText(context.getApplicationContext(),
                                     "재료가 추가되었습니다",Toast.LENGTH_SHORT).show();
                         }
+                        // GoodsActivity는 재료 갱신
                         else{
                             db.update(DBContract.GoodsEntry.TABLE_NAME, values, DBContract.GoodsEntry._ID + "=?",
                                     new String[]{String.valueOf(id)});
@@ -276,7 +318,7 @@ public class GoodsDialog extends Dialog implements View.OnClickListener {
                                     "재료가 갱신되었습니다",Toast.LENGTH_SHORT).show();
                         }
 
-
+                        // 각각이 실행된 Activity에 따라 화면에 갱신해줌
                         if (context.getClass() == FreshnessActivity.class) {
                             ((FreshnessActivity)context).setupFreshnessRecycler(); }
                         else if (context.getClass() == SectionActivity.class) {
